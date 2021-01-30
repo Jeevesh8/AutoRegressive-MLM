6a27844179056e12292cdc53a738274a2f75d088
jeevesh8 <creativityinczenyoga@gmail.com>
2021-01-30 16:16:23 +0530

    Chiisana Hosei

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -278,7 +278,7 @@ class ExtendedEncoder(hk.Module):
             w = self.embed_layer.word_emb_layer.embeddings
             logits = jnp.tensordot(y, w, (-1,-1)) + hk.get_parameter('output_bias', 
                                                                     [self.config['vocab_size']], 
-                                                                    w_init=hk.initializers.Constant(self.init_final_layer_bias()))
+                                                                    init=hk.initializers.Constant(self.init_final_layer_bias()))
         else:
             logits = hk.Linear(output_size=config['vocab_size'],)(y)
         return logits
