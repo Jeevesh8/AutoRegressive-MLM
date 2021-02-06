7a22fe4ba4704d694843d9d09d1dadc952606316
jeevesh8 <creativityinczenyoga@gmail.com>
2021-02-06 23:00:41 +0530

    Loading pre-trained weights for gru

--- src/model/utils.py
+++ src/model/utils.py
@@ -20,9 +20,10 @@ def logits_to_ar_classifier_params(pretrained_params, classifier_params):
     pretrained_params = to_mutable_dict(pretrained_params)
     pretrained_params['ar_classifier'] = pretrained_params['mlm_predictor']
     pretrained_params.pop('mlm_predictor')
-    pretrained_params['ar_classifier']['extended_encoder/linear'] = classifier_params['ar_classifier']['auto_regressive_classifier/linear']
     pretrained_params['ar_classifier'] = change_keys(pretrained_params['ar_classifier'], 'extended_encoder', 'auto_regressive_classifier')
     pretrained_params['ar_classifier'] = change_keys(pretrained_params['ar_classifier'], 'auto_regressive_classifier/~/', 'auto_regressive_classifier/')
+    #pretrained_params['ar_classifier']['extended_encoder/linear'] = classifier_params['ar_classifier']['auto_regressive_classifier/linear']
+    pretrained_params['ar_classifier']['auto_regressive_classifier/~/gru/~/gru'] = classifier_params['ar_classifier']['auto_regressive_classifier/~/gru/~/gru']
     return to_immutable_dict(pretrained_params)
 
 class Scope(object):
