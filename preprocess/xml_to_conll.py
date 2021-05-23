4bd80fac1042c34825812895bd6bfd987ef79593
Jeevesh Juneja <creativityinczenyoga@gmail.com>
2021-05-23 11:07:32 +0530

    Update xml_to_conll.py

--- preprocess/xml_to_conll.py
+++ preprocess/xml_to_conll.py
@@ -6,12 +6,12 @@ from bs4 import BeautifulSoup
 import argparse
 
 def clean_text(text: str) -> str:
-    for elem in ['.', ',','!',';', ':', '*', '?']:
-        text = text.replace(elem, ' '+elem+' ')
-    
     replaces = [("&", "and"), ("’", "'"), ("“", '"'), ("”", '"')]
     for elem in replaces:
         text = text.replace(*elem)
+    
+    for elem in ['.', ',','!',';', ':', '*', '?', '/', '\'', '\"', '[', ']', '(', ')']:
+        text = text.replace(elem, ' '+elem+' ')
     
     text = text.strip(' _\t\n')
     text = text.split('____')[0]                                                    #To remove footnotes
